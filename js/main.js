f6e9230fd4700af7d685e7e41b4e28b63906a759
Jorge Guiragossian <84877329+jorgeguiragossian@users.noreply.github.com>
2023-12-23 19:38:39 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Custom JavaScript code can go here
+
+// Example: Initializing a Bootstrap Carousel
+$(document).ready(function(){
+    $('.carousel').carousel({
+        interval: 2000
+    });
+});
