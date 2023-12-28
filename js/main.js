1337e7adc4c87fa880c0ad03371a59743fe0b4b7
Jorge Guiragossian <84877329+jorgeguiragossian@users.noreply.github.com>
2023-12-28 15:39:26 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,13 @@ $(document).ready(function(){
         interval: 2000
     });
 });
+
+
+window.addEventListener('scroll', function() {
+    var logo = document.querySelector('.navbar-brand img');
+    if (window.pageYOffset > 100) { // Adjust the 100 value as needed
+        logo.style.height = '40px'; // Smaller size when scrolled down
+    } else {
+        logo.style.height = '100px'; // Larger size when at the top of the page
+    }
+});
